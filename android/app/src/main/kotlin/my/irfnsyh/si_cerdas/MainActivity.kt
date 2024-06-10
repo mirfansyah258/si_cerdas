add717e2f5c548bc22353c23e7ba3146ec58867a
mirfansyah258 <mirfansyah258@gmail.com>
2024-06-10 17:31:41 +0700

    init commit

---
+++ android/app/src/main/kotlin/my/irfnsyh/si_cerdas/MainActivity.kt
@@ -0,0 +1,5 @@
+package my.irfnsyh.si_cerdas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const mainYellow = Color.fromRGBO(244, 223, 142, 1);
+const darkYellow = Color.fromRGBO(116, 99, 29, 1);
+const black = Color.fromRGBO(0, 0, 0, 1);
+const white = Color.fromRGBO(255, 255, 255, 1);
